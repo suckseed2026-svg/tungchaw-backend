3bf87794057051f2081f2b6de3616085cdd20a79
Vanlalawmpuia Fanai <Vanlalawmpuiafanaiopa11@gmail.com>
2026-07-24 15:11:25 +0530

    refactor: use shared inventory service in sales

--- src/sales/sales.module.ts
+++ src/sales/sales.module.ts
@@ -2,11 +2,17 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
 import { AuthorizationModule } from '../authorization/authorization.module';
 import { BusinessContextModule } from '../business-context/business-context.module';
+import { InventoryTransactionsModule } from '../inventory-transactions/inventory-transactions.module';
 import { SalesController } from './sales.controller';
 import { SalesService } from './sales.service';
 
 @Module({
-  imports: [AuthModule, BusinessContextModule, AuthorizationModule],
+  imports: [
+    AuthModule,
+    BusinessContextModule,
+    AuthorizationModule,
+    InventoryTransactionsModule,
+  ],
   controllers: [SalesController],
   providers: [SalesService],
   exports: [SalesService],
